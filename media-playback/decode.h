6749ac7c0cc6cb95da0f63cda224621d557767f6
0x1233333 <54734393+0x1233333@users.noreply.github.com>
2025-12-26 18:10:13 +0800

    Update decode.h

--- media-playback/decode.h
+++ media-playback/decode.h
@@ -4,14 +4,6 @@
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #pragma once
@@ -19,7 +11,7 @@
 #include <obs.h>
 #include <obs-module.h>
 
-// [修改] 改回标准写法，这需要我们在 CMake 中正确配置 libobs 目录
+// [关键] 使用标准引用，依赖 CMake 传入的绝对路径
 #include <util/circlebuf.h>
 
 extern "C" {
